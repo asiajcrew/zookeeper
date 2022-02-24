3614a345b3e48356bd4e1dded6cdae3833876c38
asia <asia_jcrew96@yahoo.com>
2022-02-24 07:52:25 -0500

    testing deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.JSON');
+const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
